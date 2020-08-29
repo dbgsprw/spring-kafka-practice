0d7881b32ea008a62b6fbc120bebf68ac69a02cc
dbgsprw <dbgsprw@gmail.com>
2020-08-30 05:08:36 +0900

    Initial commit

---
+++ src/test/java/dbgsprw/springkafkapractice/SpringKafkaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package dbgsprw.springkafkapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringKafkaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
